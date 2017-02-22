c94bff856ce765fac07dbab380a4adea906ee5ed
inponomarev <iponomarev@mail.ru>
2017-02-22 23:45:30 +0300

    fix maven

--- src/main/java/ru/curs/lyragridtest/Settings.java
+++ src/main/java/ru/curs/lyragridtest/Settings.java
@@ -1,5 +1,6 @@
 package ru.curs.lyragridtest;
 
+import java.io.File;
 import java.util.Properties;
 
 public class Settings {
@@ -7,12 +8,19 @@ public class Settings {
 
 	static {
 		// put your settings here
-		settings.setProperty("score.path", "d:/workspaces/mvnspace-neon/lyragridtest/src/main/celesta");
+		settings.setProperty("score.path", getMyPath() + "../src/main/celesta");
 		settings.setProperty("pylib.path", "d:/jython2.7.0/Lib");
 		settings.setProperty("database.connection",
 				"jdbc:postgresql://127.0.0.1:5432/kladr?user=postgres&password=123");
 		// p.setProperty("database.connection",
 		// "jdbc:sqlserver://localhost;databaseName=testkladr;user=sa;password=123");
+	}
+
+	static String getMyPath() {
+		String path = Settings.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		File f = new File(path.replace("%20", " "));
+		return f.getParent() + File.separator;
+
 	}
 
 	public static Properties getSettings() {
